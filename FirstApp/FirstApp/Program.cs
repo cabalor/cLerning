8c501c7478124cc03f66c23327b19adbfa4b1474
Cab <cabalor@gmail.com>
2018-07-03 18:16:11 +0200

    vars

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -11,6 +11,16 @@ namespace FirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+
+            byte number =  4;
+            int num = 333;
+            long dd;
+            const float Pi = 3.15f;
+            decimal big = 3.5657765m;
+            double dob = 4.5;
+            char s = 's';
+            Console.WriteLine("number"+number+" "+dob+" "+big+" "+Pi+" "+num+" "+s);
+            Console.WriteLine(dob);
         }
     }
 }
