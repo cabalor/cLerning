17cb3260dba3da782ba46fcbc9d32a6e5e8ee479
Cab <cabalor@gmail.com>
2018-07-19 18:51:29 +0200

    nowosci

--- FirstApp/FirstApp/Wybieracz.cs
+++ FirstApp/FirstApp/Wybieracz.cs
@@ -15,6 +15,11 @@ namespace FirstApp
 
     }
 
+    class Ludzik{
+
+        public int wiek;
+        }
+
 
     class Wybieracz
     {
@@ -29,8 +34,6 @@ namespace FirstApp
             var cosId = 3;
 
             Console.WriteLine((Things)cosId);
-
-
             Console.WriteLine(cos.ToString());
 
             var enumek = "Kompa";
@@ -38,6 +41,26 @@ namespace FirstApp
             var zamiana = (Things)Enum.Parse(typeof(Things), enumek);
 
             Console.WriteLine(zamiana);
+
+
+            int i = 10;
+            var b = i;
+            b++;
+            Console.WriteLine(string.Format("a: {0}, b: {1}", i, b));
+
+            var ar1 = new int[3] {1, 2, 3 };
+            var ar2 = ar1;
+
+            for(int x =0; x< ar2.Length; x++)
+            {
+                ar2[x]++;
+            }
+
+            Console.WriteLine(string.Format("pierwszy: {0}, drugi: {1}, trzeci:{2}", ar1[0], ar1[1], ar1[2]));
+            Console.WriteLine(string.Format("pierwszy: {0}, drugi: {1}, trzeci:{2}", ar2[0], ar2[1], ar2[2]));
+
+
+            var ludek = new Ludzik {wiek = 20 };
 
         }
 
