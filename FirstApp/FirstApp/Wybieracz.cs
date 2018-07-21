819732607faa33766622d2b4f65be1f713e2702b
Cab <cabalor@gmail.com>
2018-07-21 12:09:31 +0200

    nowosci aaA

--- FirstApp/FirstApp/Wybieracz.cs
+++ FirstApp/FirstApp/Wybieracz.cs
@@ -15,10 +15,10 @@ namespace FirstApp
 
     }
 
-    class Ludzik{
+    class Ludzik {
 
         public int wiek;
-        }
+    }
 
 
     class Wybieracz
@@ -48,10 +48,10 @@ namespace FirstApp
             b++;
             Console.WriteLine(string.Format("a: {0}, b: {1}", i, b));
 
-            var ar1 = new int[3] {1, 2, 3 };
+            var ar1 = new int[3] { 1, 2, 3 };
             var ar2 = ar1;
 
-            for(int x =0; x< ar2.Length; x++)
+            for (int x = 0; x < ar2.Length; x++)
             {
                 ar2[x]++;
             }
@@ -60,10 +60,74 @@ namespace FirstApp
             Console.WriteLine(string.Format("pierwszy: {0}, drugi: {1}, trzeci:{2}", ar2[0], ar2[1], ar2[2]));
 
 
-            var ludek = new Ludzik {wiek = 20 };
+            var ludek = new Ludzik { wiek = 20 };
+
+
+            var rok = Rok.jesien;
+
+            switch (rok)
+            {
+                case Rok.wiosna:
+                    Console.WriteLine("mamy wiosen");
+                    break;
+                case Rok.jesien:
+                    Console.WriteLine("mamy jesien");
+                    break;
+            }
+
+
+            int dana = 6;
+
+            if (dana > 7)
+            {
+                Console.WriteLine("jestem duza");
+            }
+            else
+            {
+                Console.WriteLine("jestem mala");
+            }
+
+
+            for (int gg = 1; gg < 10; gg++) {
+                if(gg%2 == 0)
+                {
+                    Console.WriteLine(gg);
+                }
+
+            }
+            for (int gg = 10; gg > 0; gg--)
+            {
+                if (gg % 2 == 0)
+                {
+                    Console.WriteLine(gg);
+                }
+
+            }
+
+
+            var imie = "ziomus";
+            for(int gg =0; gg< imie.Length; gg++)
+            {
+                Console.WriteLine(imie[gg]);
+            }
+            Console.WriteLine(" ");
+            foreach(char litera in imie)
+            {
+                Console.WriteLine(litera);
+            }
+
+            var liczby = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17 };
+
+            foreach (int liczba in liczby)
+            {
+                Console.WriteLine(liczba);
+            }
+           
+
+            
+
+
 
         }
-
-
     }
 }
