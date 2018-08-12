b6b4671bab39f1e775290415bef8be9844b9a64c
Cab <cabalor@gmail.com>
2018-08-12 11:44:26 +0200

    properites

--- FirstApp/FirstApp/Programik.cs
+++ FirstApp/FirstApp/Programik.cs
@@ -54,6 +54,51 @@ namespace FirstApp
         }
     }
 
+
+    public class Czolwiek
+    {
+        private String imie;
+
+        public String getImie()
+        {
+            return imie;
+        }
+
+        public void setImie(String imie)
+        {
+            this.imie = imie;
+        }
+        public DateTime urodziny { get; set; }
+
+        public int wiek {
+
+            get { var roznica = DateTime.Today - urodziny;
+                var lata = roznica.Days / 365;
+                return lata;
+            }
+
+        }
+
+
+        public Czolwiek(DateTime uro)
+        {
+            urodziny = uro;
+        }
+
+
+
+
+        public override String ToString()
+        {
+            return "imie " + imie;
+        }
+
+
+    }
+
+
+
+
     class Programik
     {
         static void Main(String[] args)
@@ -90,6 +135,11 @@ namespace FirstApp
                 Console.WriteLine("problem");
             }
 
+
+            var czlowien = new Czolwiek(new DateTime(1965, 1, 1));
+            czlowien.setImie("turbo");
+            Console.WriteLine(czlowien.ToString());
+            Console.WriteLine(czlowien.wiek);
 
 
 
