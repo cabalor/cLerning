3fd8edcbec169a51e52a3061cbba5bfc4c76a848
cbl <cabalor@gmail.com>
2018-10-22 21:27:53 +0200

    news

---
+++ FirstApp/FirstApp/FileLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+namespace FirstApp
+{
+    public class FileLogger : ILoger
+    {
+        private readonly string path;
+
+        public FileLogger(string path)
+        {
+            this.path = path;
+        }
+
+        
+
+        public void LogError(string msg)
+        {
+            Log(msg, "Err");
+        }
+
+        public void logInfo(string msg)
+        {
+            Log(msg, "Err");
+        }
+
+        private void Log(string msg, string msgType)
+        {
+            using (var streamWriter = new StreamWriter(path, true))
+            {
+                streamWriter.WriteLine(msgType + DateTime.Now + " " + msg);
+            }
+        }
+
+
+    }
+}
+
